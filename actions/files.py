d6edcbe9527b3659bfcec2409f45c1793de3d7e1
Farfw <a.sarsengalieva@innopolis.university>
2025-07-04 19:29:57 +0300

    Delete actions directory

--- actions/files.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-
-def edit_file(path):
-    try:
-        os.startfile(path)
-        logging.info(f"Файл открыт: {path}")
-    except Exception as e:
-        logging.error(f"Ошибка при открытии файла: {e}")
